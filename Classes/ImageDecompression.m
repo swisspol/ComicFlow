2145e97954fb971c7a2ab3b1c61a8a6d317984ac
Pierre-Olivier Latour <pol@mac.com>
2016-01-05 15:24:23 -0800

    Removed support for libjpeg-turbo

--- Classes/ImageDecompression.m
+++ Classes/ImageDecompression.m
@@ -16,26 +16,14 @@
 #undef __STRICT_ANSI__  // Work around WEBP_INLINE not defined as "inline"
 
 #import <sys/sysctl.h>
-#import <jerror.h>
-#import <jpeglib.h>
 #import <webp/decode.h>
 #import <ImageIO/ImageIO.h>
 
 #import "ImageDecompression.h"
 #import "ImageUtilities.h"
 
-#define __USE_LIBJPEG_TURBO__ 0
 #define __USE_RGBX_JPEG__ 0  // RGB appears a bit faster than RGBX on iPad Mini
 #define __USE_RGBA_WEBP__ 0  // RGB appears a bit faster than RGBA on iPad Mini
-
-#if __USE_LIBJPEG_TURBO__
-
-typedef struct {
-  struct jpeg_error_mgr error_mgr;
-  jmp_buf jmp_buffer;
-} ErrorManager;
-
-#endif
 
 BOOL IsImageFileExtensionSupported(NSString* extension) {
   return ![extension caseInsensitiveCompare:@"jpg"] || ![extension caseInsensitiveCompare:@"jpeg"] ||
@@ -46,117 +34,6 @@ BOOL IsImageFileExtensionSupported(NSString* extension) {
 static void _ReleaseDataCallback(void* info, const void* data, size_t size) {
   free(info);
 }
-
-#if __USE_LIBJPEG_TURBO__
-
-// Replace default implementation to log error and use longjmp() instead of simply calling exit()
-static void _ErrorExit(j_common_ptr cinfo) {
-  ErrorManager* errorManager = (ErrorManager*)cinfo->err;
-  
-  char buffer[JMSG_LENGTH_MAX];
-  (*errorManager->error_mgr.format_message)(cinfo, buffer);
-  XLOG_ERROR(@"libjpeg error (%i): %s", errorManager->error_mgr.msg_code, buffer);
-  
-  if (cinfo->err->msg_code != JERR_UNKNOWN_MARKER) {
-    longjmp(errorManager->jmp_buffer, 1);
-  }
-}
-
-static void _EmitMessage(j_common_ptr cinfo, int msg_level) {
-  ErrorManager* errorManager = (ErrorManager*)cinfo->err;
-  
-  if (msg_level < 0) {  // Indicates a corrupt-data warning
-    char buffer[JMSG_LENGTH_MAX];
-    (*errorManager->error_mgr.format_message)(cinfo, buffer);
-    XLOG_WARNING(@"libjpeg warning (%i): %s", errorManager->error_mgr.msg_code, buffer);
-    if ((errorManager->error_mgr.msg_code == JWRN_EXTRANEOUS_DATA) && (errorManager->error_mgr.msg_parm.i[1] == 0xD9)) {
-      // Extraneous bytes before EOI marker should be acceptable (e.g. Sony Ericsson P990i)
-    } else {
-      longjmp(errorManager->jmp_buffer, 1);  // Abort on corrupt-data
-    }
-  } else if (msg_level == 0) {  // Indicates an advisory message
-    char buffer[JMSG_LENGTH_MAX];
-    (*errorManager->error_mgr.format_message)(cinfo, buffer);
-    XLOG_INFO(@"libjpeg message (%i): %s", errorManager->error_mgr.msg_code, buffer);
-  }
-}
-
-static CGImageRef _CreateCGImageFromJPEGData(NSData* data, CGSize targetSize, BOOL fillMode) {
-  void* buffer = NULL;
-  struct jpeg_decompress_struct dinfo;
-  ErrorManager errorManager;
-  jpeg_create_decompress(&dinfo);
-  dinfo.err = jpeg_std_error(&errorManager.error_mgr);
-  errorManager.error_mgr.error_exit = _ErrorExit;
-  errorManager.error_mgr.emit_message = _EmitMessage;
-  if (setjmp(errorManager.jmp_buffer)) {
-    if (buffer) {
-      free(buffer);
-    }
-    jpeg_destroy_decompress(&dinfo);
-    return NULL;
-  }
-  jpeg_mem_src(&dinfo, (unsigned char*)data.bytes, data.length);
-  jpeg_read_header(&dinfo, true);  // This sets dinfo.scale_num and dinfo.scale_denom to 1
-  
-  size_t width = dinfo.image_width;
-  size_t height = dinfo.image_height;
-  unsigned int target_size = MAX(targetSize.width, targetSize.height);
-  if (fillMode) {
-    unsigned int min_size = MIN(width, height);
-    if (min_size > target_size) {
-      dinfo.scale_denom = MIN(1 << (unsigned int)log2f((float)min_size / target_size), 8);
-    }
-  } else {
-    unsigned int max_size = MAX(width, height);
-    if (max_size > target_size) {
-      dinfo.scale_denom = MIN(1 << (unsigned int)log2f((float)max_size / target_size), 8);
-    }
-  }
-  jpeg_calc_output_dimensions(&dinfo);
-  
-  size_t rowBytes = 4 * dinfo.output_width;
-  if (rowBytes % 16) {
-    rowBytes = ((rowBytes / 16) + 1) * 16;
-  }
-  size_t size = dinfo.output_height * rowBytes;
-  buffer = malloc(size);
-  if (buffer == NULL) {
-    XLOG_ERROR(@"Failed allocating memory for JPEG buffer");
-    jpeg_destroy_decompress(&dinfo);
-    return NULL;
-  }
-  dinfo.dct_method = JDCT_IFAST;
-#if __USE_RGBX_JPEG__
-  dinfo.out_color_space = JCS_EXT_RGBX;
-#else
-  dinfo.out_color_space = JCS_RGB;
-#endif
-  jpeg_start_decompress(&dinfo);
-  JDIMENSION scanline = 0;
-  unsigned char* base_address = (unsigned char*)buffer;
-  while (scanline < dinfo.output_height) {
-    JDIMENSION lines = jpeg_read_scanlines(&dinfo, (JSAMPARRAY)&base_address, dinfo.rec_outbuf_height);
-    scanline += lines;
-    base_address += lines * rowBytes;
-  }
-  jpeg_finish_decompress(&dinfo);
-  jpeg_destroy_decompress(&dinfo);
-  
-  CGDataProviderRef provider = CGDataProviderCreateWithData(buffer, buffer, size, _ReleaseDataCallback);
-  CGColorSpaceRef colorspace = CGColorSpaceCreateDeviceRGB();
-#if __USE_RGBX_JPEG__
-  CGImageRef imageRef = CGImageCreate(dinfo.output_width, dinfo.output_height, 8, 32, rowBytes, colorspace, kCGImageAlphaNoneSkipLast, provider, NULL, true, kCGRenderingIntentDefault);
-#else
-  CGImageRef imageRef = CGImageCreate(dinfo.output_width, dinfo.output_height, 8, 24, rowBytes, colorspace, kCGImageAlphaNone, provider, NULL, true, kCGRenderingIntentDefault);
-#endif
-  CGColorSpaceRelease(colorspace);
-  CGDataProviderRelease(provider);
-  
-  return imageRef;
-}
-
-#endif
 
 static CGImageRef _CreateCGImageFromWebPData(NSData* data) {
   static uint32_t cores = 0;
@@ -233,11 +110,6 @@ static CGImageRef _CreateCGImageFromData(NSData* data) {
 CGImageRef CreateCGImageFromFileData(NSData* data, NSString* extension, CGSize targetSize, BOOL thumbnailMode) {
   CFTimeInterval time = CFAbsoluteTimeGetCurrent();
   CGImageRef imageRef = NULL;
-#if __USE_LIBJPEG_TURBO__
-  if (![extension caseInsensitiveCompare:@"jpg"] || ![extension caseInsensitiveCompare:@"jpeg"]) {
-    imageRef = _CreateCGImageFromJPEGData(data, targetSize, thumbnailMode);
-  } else
-#endif
   if (![extension caseInsensitiveCompare:@"webp"]) {
     imageRef = _CreateCGImageFromWebPData(data);
   } else {
